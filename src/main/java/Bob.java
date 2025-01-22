7a461da3b3290b378b59c65ab6c6bcac553324c1
awlarpi <64833276+awlarpi@users.noreply.github.com>
2025-01-22 23:32:31 +0800

    Refactor Bob to use Task class for task management and add mark/unmark functionality

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 public class Bob {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> tasks = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
         System.out.println("Hello! I'm Bob");
         System.out.println("What can I do for you?");
 
@@ -16,9 +16,28 @@ public class Bob {
                 for (int i = 0; i < tasks.size(); i++) {
                     System.out.println((i + 1) + ". " + tasks.get(i));
                 }
+            } else if (input.startsWith("mark ")) {
+                // grab the number following "mark "
+                int taskNumber = Integer.parseInt(input.substring(5));
+
+                // mark the task as done
+                tasks.get(taskNumber - 1).markAsDone();
+
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(tasks.get(taskNumber - 1));
+            } else if (input.startsWith("unmark")) {
+                // grab the number following "unmark "
+                int taskNumber = Integer.parseInt(input.substring(7));
+
+                // mark the task as not done
+                tasks.get(taskNumber - 1).markAsNotDone();
+
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println(tasks.get(taskNumber - 1));
             } else {
-                tasks.add(input);
-                System.out.println("added: " + input);
+                Task task = new Task(input);
+                tasks.add(task);
+                System.out.println("added: " + task);
             }
         }
 
