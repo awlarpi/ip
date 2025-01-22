6d6a1c8a16dc93fadf87f0bbb086d7b749470be3
awlarpi <64833276+awlarpi@users.noreply.github.com>
2025-01-22 23:15:03 +0800

    Add echo functionality to Duke

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
+
 public class Bob {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Hello! I'm Bob");
         System.out.println("What can I do for you?");
+        
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            System.out.println(input);
+        }
+        
         System.out.println("Bye. Hope to see you again soon!");
+        scanner.close();
     }
 }
