b5484eeb77988b9f755c706bbd378a94e23c23d2
awlarpi <64833276+awlarpi@users.noreply.github.com>
2025-01-22 23:19:31 +0800

    Add ability to store and display user input

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,19 +1,27 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Bob {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> tasks = new ArrayList<>();
         System.out.println("Hello! I'm Bob");
         System.out.println("What can I do for you?");
-        
+
         while (true) {
             String input = scanner.nextLine();
             if (input.equals("bye")) {
                 break;
+            } else if (input.equals("list")) {
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println((i + 1) + ". " + tasks.get(i));
+                }
+            } else {
+                tasks.add(input);
+                System.out.println("added: " + input);
             }
-            System.out.println(input);
         }
-        
+
         System.out.println("Bye. Hope to see you again soon!");
         scanner.close();
     }
