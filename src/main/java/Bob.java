592fc64024f280dddfbdd8f88e4d07bc432ae41d
awlarpi <64833276+awlarpi@users.noreply.github.com>
2025-01-22 23:11:22 +0800

    Rename Duke to Bob, and add greet functionality

---
+++ src/main/java/Bob.java
@@ -0,0 +1,7 @@
+public class Bob {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Bob");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
